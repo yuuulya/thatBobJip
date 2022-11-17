597bc29f9b4ba4f29e038bcff7d3a2b876fb0cbc
yuuulya <yla101@naver.com>
2022-11-17 14:16:39 +0900

    FEAT : API Call, Draw a mark in the map
    - 새싹 건물 기준 반경 1km 범위 녹색원으로 표시 (디자인 추후 변경)
    - 좌표계 기준이 달라 카카오 지도 api를 활용하여 변환을 해주어 위도경도에 따른 mark 표시
      : 하지만, 오차가 발생하여 이를 일정 숫자를 더해주는 형식으로 수정함
    - "영업","폐업" / "종로구"(임시) 필터링 적용
    - 호출 건수가 한 번에 1,000건으로 제한되어 있어서 반경을 3.3km로 지정하여 필터링이 되는지 확인
    - 확인 결과 상태가 "영업"을 기준으로 "종로구"에 위치한 가게만 조회됨
    - (수정 필요)1,000건을 하나씩 조회하면서 결과가 한 번에 띄는 것이 아니라 개별적으로 mark가 하나씩 찍힘
    - (수정 필요)호출이 1,000건씨만 가능하여 json에 들어있는 모든 자료 조회가 한번에 이루어지지 못함

--- js/map.js
+++ js/map.js
@@ -1,49 +1,49 @@
-let centerLat = 37.54667362412562;
-let centerLng = 126.94970599817853;
-var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-  mapOption = {
-    center: new kakao.maps.LatLng(centerLat, centerLng), // 지도의 중심좌표 (공덕역)
-    level: 3, // 지도의 확대 레벨
-  };
+// let centerLat = 37.54667362412562;
+// let centerLng = 126.94970599817853;
+// // var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+// //   mapOption = {
+// //     center: new kakao.maps.LatLng(centerLat, centerLng), // 지도의 중심좌표 (공덕역)
+// //     level: 5, // 지도의 확대 레벨
+// //   };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+// // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+// var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 지도에 표시할 원을 생성
-var circle = new kakao.maps.Circle({
-  center: new kakao.maps.LatLng(centerLat, centerLng), // 원의 중심좌표햣 (공덕역)
-  radius: 1000, // 미터 단위의 원의 반지름
-  strokeWeight: 2, // 선의 두께
-  strokeColor: "#75fa75", // 선의 색깔
-  strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명
-  strokeStyle: "dashed", // 선의 스타일 입니다
-  fillColor: "#d0ffcf", // 채우기 색깔입니다
-  fillOpacity: 0.5, // 채우기 불투명도 입니다
-});
+// // 지도에 표시할 원을 생성
+// var circle = new kakao.maps.Circle({
+//   center: new kakao.maps.LatLng(centerLat, centerLng), // 원의 중심좌표햣 (공덕역)
+//   radius: 1000, // 미터 단위의 원의 반지름
+//   strokeWeight: 2, // 선의 두께
+//   strokeColor: "#75fa75", // 선의 색깔
+//   strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명
+//   strokeStyle: "dashed", // 선의 스타일 입니다
+//   fillColor: "#d0ffcf", // 채우기 색깔입니다
+//   fillOpacity: 0.3, // 채우기 불투명도 입니다
+// });
 
-console.log("원표시");
-// 지도에 원을 표시합니다
-circle.setMap(map);
+// console.log("원표시");
+// // 지도에 원을 표시합니다
+// circle.setMap(map);
 
-var geocoder = new kakao.maps.services.Geocoder(), // 좌표계 변환 객체를 생성합니다
-  wtmX = 194986.978304208 + 70;
-console.log(wtmX); // 변환할 WTM X 좌표 입니다
-wtmY = 448633.912588529 + 300; // 변환할 WTM Y 좌표 입니다
+// var geocoder = new kakao.maps.services.Geocoder(), // 좌표계 변환 객체를 생성합니다
+//   wtmX = 194986.978304208 + 70;
+// console.log(wtmX); // 변환할 WTM X 좌표 입니다
+// wtmY = 448633.912588529 + 300; // 변환할 WTM Y 좌표 입니다
 
-// WTM 좌표를 WGS84 좌표계의 좌표로 변환합니다
-geocoder.transCoord(wtmX, wtmY, transCoordCB, {
-  input_coord: kakao.maps.services.Coords.WTM, // 변환을 위해 입력한 좌표계 입니다
-  output_coord: kakao.maps.services.Coords.WGS84, // 변환 결과로 받을 좌표계 입니다
-});
+// // WTM 좌표를 WGS84 좌표계의 좌표로 변환합니다
+// geocoder.transCoord(wtmX, wtmY, transCoordCB, {
+//   input_coord: kakao.maps.services.Coords.WTM, // 변환을 위해 입력한 좌표계 입니다
+//   output_coord: kakao.maps.services.Coords.WGS84, // 변환 결과로 받을 좌표계 입니다
+// });
 
-// 좌표 변환 결과를 받아서 처리할 콜백함수 입니다.
-function transCoordCB(result, status) {
-  // 정상적으로 검색이 완료됐으면
-  if (status === kakao.maps.services.Status.OK) {
-    // 마커를 변환된 위치에 표시합니다
-    var marker = new kakao.maps.Marker({
-      position: new kakao.maps.LatLng(result[0].y, result[0].x), // 마커를 표시할 위치입니다
-      map: map, // 마커를 표시할 지도객체입니다
-    });
-  }
-}
+// // 좌표 변환 결과를 받아서 처리할 콜백함수 입니다.
+// function transCoordCB(result, status) {
+//   // 정상적으로 검색이 완료됐으면
+//   if (status === kakao.maps.services.Status.OK) {
+//     // 마커를 변환된 위치에 표시합니다
+//     var marker = new kakao.maps.Marker({
+//       position: new kakao.maps.LatLng(result[0].y, result[0].x), // 마커를 표시할 위치입니다
+//       map: map, // 마커를 표시할 지도객체입니다
+//     });
+//   }
+// }
